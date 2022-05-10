25a59f864ddfb24cdd28e9965eb4d84cd5baca72
juanrabaro <jrbaro@iesrafaelalberti.es>
2022-05-10 13:48:47 +0200

    tercer commit con segundo archivo modificado

--- carpeta1/ej2.py
+++ carpeta1/ej2.py
@@ -1,17 +1,30 @@
+def es_par(numero):
+    return numero % 2 == 0
 
-def lista_dic(lista_diccionarios):
-    lista_nueva = []
-    lista_provisional = []
-    for diccionarios in lista_diccionarios:
-        for claves in diccionarios:
-            if claves in lista_provisional:
-                if claves not in lista_nueva:
-                    lista_nueva.append(claves)
-            if claves not in lista_provisional:
-                lista_provisional.append(claves)
-    return lista_nueva
+# Carga valores en formato matriz
+matriz = [ [ 1, 2, 3],
+           [ 1, 2, 3],
+           [ 1, 2, 3] ]
+
+# Creamos una nueva matriz (ojo, solo la lista inicial)
+# Será una matriz cuando vayamos añadiéndole listas
+pares = [ ]
+
+# Muestra la matriz, pero a la vez crea una nueva matriz con el resultado de la
+# función es_par aplicado a cada valor de la matriz original
+for fila in matriz:
+    nueva_fila = []
+    for celda in fila:
+        print(celda, end=" ")
+        nueva_fila.append(es_par(celda))
+    pares.append(nueva_fila)
+    print()
+
+# Muestra la matriz por consola
+for fila in pares:
+    for celda in fila:
+        print(celda, end=" ")
+    print()
 
 
-if __name__=="__main__":
-    lista_diccionarios = [{"a":1, "b":2}, {"a":3, "b":4}, {"a":5, "f":3}]
-    print(lista_dic(lista_diccionarios))
+
